4edf64a412700ab3a0fa181de05c9ae92c3ab33a
pieteradejong <pieter.a.dejong@gmail.com>
2015-11-07 14:42:40 -0800

    using complex() - added Mandelbrot set membership chek - added unit testing function

--- mandelbrot/mandelbrot.py
+++ mandelbrot/mandelbrot.py
@@ -6,10 +6,30 @@ def generateComplexNumber():
   random.seed()
   real = random.randint(-100, 100)
   compl = random.randint(-100, 100)
-  c = (real, compl)
+  c = complex(real, compl)
   return c
 
 
-print(generateComplexNumber())
-print(generateComplexNumber())
+def isMandelbrot(c):
+  z = 0
+  for i in range(100):
+    z = pow(z, 2) + c
+    absZ = abs(z)
+    if absZ > 2:
+      return False
+  print "Is Mandelbrot!"
+  return True
+
+
+
+def runUnitTests():
+  z = generateComplexNumber()
+  print z
+  isMandelbrot(z)
+  z = generateComplexNumber()
+  print z
+  isMandelbrot(z)
+
+  print isMandelbrot(complex(-1, 0))
+  print isMandelbrot(complex(1, 0))
 
