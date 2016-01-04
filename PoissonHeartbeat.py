3971b1f6a51999ef686b9ef5c4214bc244502cda
pieteradejong <pieter.a.dejong@gmail.com>
2016-01-03 19:08:01 -0500

    events and lambda param are now instance vars

--- PoissonHeartbeat.py
+++ PoissonHeartbeat.py
@@ -2,10 +2,11 @@ import time
 import random
 
 class PoissonHeartbeat:
-  def __init__(self, runtime=10):
-    self.lambd = 0.1
-    intervals = [random.expovariate(self.lambd) for i in range(runtime)]
-    print intervals
+  def __init__(self, events=10, lambd=0.1):
+    self.events = events
+    self.lambd = lambd
+    intervals = [random.expovariate(self.lambd) for i in range(self.events)]
+    print intervals, "\n"
     for v in intervals:
       time.sleep(v)
       print "ping! after interval ", v
