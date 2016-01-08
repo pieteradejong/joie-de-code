ab0b2ca458a7709774e985316ba51988dc969388
pieteradejong <pieter.a.dejong@gmail.com>
2016-01-08 10:11:53 -0800

    expanded form, added jQuery handler code

--- strdistanceapp/app.py
+++ strdistanceapp/app.py
@@ -1,13 +1,22 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return url_for('static', filename='style.css')
+def index():
+    # return url_for('static', filename='style.css')
+    # return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def distance():
   return 123
+
+  str1 =  request.form['username'];
+  str2 = request.form['password'];
+  distance = get_distance(str1, str2)
+  
+  return json.dumps({'status':'OK','distance':distance});
 
 if __name__ == '__main__':
     app.run()
--- strdistanceapp/script.js
+++ strdistanceapp/script.js
@@ -1,8 +1,51 @@
-function handleClick() {
-  // get value str1, str2
+$( "form" ).submit(function( event ) {
+      event.preventDefault();
+      var str1 = $("input[name='str1']").val();
+      var str2 = $("input[name='str2']").val();
+        $.ajax({
+            url: '/submit',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+
+function onSubmit() {
   var str1 = document.getElementById("str1").value;
   var str2 = document.getElementById("str2").value;
+  
   // submit reqest to app
-  // take return value and set to contents of answer div
-  document.getElementById("distance").textContent = "123";
+  var data = {
+    'str1': str1,
+    'str2': str2
+  };
+  submitAsyncPost(data);
 }
+
+function submitAsyncPost(data) {
+    var xmlhttpRequest = new XMLHttpRequest();
+    
+    xmlhttpRequest.onreadystatechange = function() {
+        if (xmlhttpRequest.readyState == XMLHttpRequest.DONE ) {
+           if(xmlhttpRequest.status == 200){
+               document.getElementById("distance").innerHTML = xmlhttpRequest.responseText;
+           }
+           else if(xmlhttpRequest.status == 400) {
+              alert('There was an error 400')
+           }
+           else {
+               alert('error returned (other than 400)')
+           }
+        }
+    }
+
+    xmlhttpRequest.open('POST', '/submit', true);
+    xmlhttpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    xmlhttpRequest.send(data);
+}
+
