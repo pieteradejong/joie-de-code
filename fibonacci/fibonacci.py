77dd197940f0c6b585cdb4a2401533d68cc569cd
pieteradejong <pieter.a.dejong@gmail.com>
2017-10-27 19:08:29 -0700

    calcuate fib numbers using python generator

---
+++ fibonacci/fibonacci.py
@@ -0,0 +1,75 @@
+from fibonacci_series import FIBONACCI_SERIES
+
+def fibonacci_generator(n):
+  a, b = 0, 1
+  yield a
+  yield b
+  while True:
+    a, b = b, a + b
+    yield b
+
+def fibonacci_print(n):
+  if n < 1:
+    raise ValueError("n must be at least 1")
+
+  fib_gen = fibonacci_generator(n)
+  i = 1
+  while i <= n:
+    next_fib = next(fib_gen, -1)
+    if next_fib == -1:
+      return
+    print next_fib
+    i += 1
+
+def fibonacci_to_list(n):
+  if n < 1:
+    raise ValueError("n must be at least 1")
+
+  fib_gen = fibonacci_generator(n)
+  fib_numbers = [None] * n
+
+  i = 1
+  while i <= n:
+    next_fib = next(fib_gen, -1)
+    if next_fib == -1:
+      return
+    fib_numbers[i-1] = next_fib
+    i += 1
+  return fib_numbers
+
+def validate_fib(generated_list, source_of_truth_list):
+  for i in xrange(min(len(generated_list), len(source_of_truth_list))):
+    if generated_list[i] != source_of_truth_list[i]:
+      return False
+  return True
+
+
+# fibonacci(0) # error
+# fibonacci_print(1) # 0
+# print "\n"
+# fibonacci_print(2) # 0, 1
+# print "\n"
+# fibonacci_print(3) # 1, 1
+# print "\n"
+# fibonacci_print(10)# 0, 1, 1, 2, 3, ...
+# print "\n"
+# print fibonacci_to_list(0) # PASSED
+print "\n"
+print fibonacci_to_list(1)
+print "\n"
+print fibonacci_to_list(2)
+print "\n"
+print fibonacci_to_list(10)
+
+
+
+print validate_fib(fibonacci_to_list(3), FIBONACCI_SERIES) # True
+print validate_fib(fibonacci_to_list(10), FIBONACCI_SERIES) # True
+print validate_fib([1,9,4], FIBONACCI_SERIES) # False
+print validate_fib([0,0,1,1,2,3], FIBONACCI_SERIES) # False
+print validate_fib([1,0,1,1,2,3], FIBONACCI_SERIES) # False
+
+
+
+
+
