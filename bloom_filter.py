4ff9343ab0c9b9126c0892e4a8d1840b786fe832
Pieter de Jong <pieter.a.dejong@gmail.com>
2018-06-09 16:47:29 -0400

    added tests

--- bloom_filter.py
+++ bloom_filter.py
@@ -18,10 +18,11 @@ class BloomFilter:
         # self.hashes = [mmh3.hash, fnv1a_32, hashlib]
         self.hashes = [mmh3.hash]
     
-    def add_string(self, s):
+    def add_item(self, s):
         for h in self.hashes:
             index = h(s) % self.size
             self.array[index] = True
+        print self.array
 
     def is_present(self, s):
         for h in self.hashes:
@@ -35,27 +36,34 @@ class BloomFilter:
         return self.size
 
     def get_approx_false_positive_rate(self):
-        passdef
+        pass
 
 
     def optimize_for_error_rate(self, n):
-        passdef
+        pass
 
 
 class BloomFilterTest(unittest.TestCase):
+    
+    TEST_STRINGS = ['coffee']
+
     def setUp(self):
         print("Setting up...")
         self.bf = BloomFilter(10)
 
     def tearDown(self):
-        # self.bf.dispose()
-        self.bf = None
+        pass
 
     def test_has_accurate_size(self):
         assert self.bf.get_size() == 10
 
-    def test_item_not_added_is_not_present(self):
-        assert self.bf.is_present('cofffee') is False
+    def test_item_not_present_when_no_items_inserted(self):
+        assert self.bf.is_present(BloomFilterTest.TEST_STRINGS[0]) is False
+
+    def test_item_present_after_only_that_item_inserted(self):
+        self.bf.add_item(BloomFilterTest.TEST_STRINGS[0])
+        assert self.bf.is_present(BloomFilterTest.TEST_STRINGS[0]) is True
 
 if __name__ == '__main__':
     unittest.main()
+
