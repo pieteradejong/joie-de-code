a580b1383b6e9289789de9dc4c94e884186b805a
Pieter de Jong <pieter.a.dejong@gmail.com>
2018-06-12 18:16:46 -0400

    added tests and some methods

--- linked_list.py
+++ linked_list.py
@@ -3,22 +3,31 @@ import unittest
 class LinkedList(object):
 	def __init__(self):
 		self.head = None
-		self.tail = None
 
 	def get_head(self):
 		return self.head
 
 	def get_tail(self):
-		return self.tail
+		pass
+		#TODO: walk to node that is tail
 
 	def add_to_head(self, value):
-		pass
+		new_node = Node(value)
+		new_node.next = self.get_head()
+		self.head = new_node
+		return get_head
 
 	def add_to_tail(self, value):
-		pass
+		tail = self.get_tail()
+		
 
-	def add_at_position(self, value):
-		pass
+	def add_at_position(self, pos):
+		assert pos >= 0
+
+		head = self.head()
+		if not head:
+
+
 
 	def get_node_by_position(self, value):
 		pass
@@ -26,23 +35,30 @@ class LinkedList(object):
 	def get_node_by_value_first(self, value):
 		pass
 
-	
-
 
 class Node(object):
 	def __init__(self, value = None, next = None):
 		self.value = value
 		self.next = next
 
-
-
-
 class LinkedListTest(unittest.TestCase):
 	def setUp(self):
-		pass
+		ll = LinkedList()
 
 	def tearDown(self):
-		pass
+		ll = None
+
+	def test_node_is_added_to_head(self):
+		ll.add_to_head(4)
+		assert ll.get_head().value == 4
+
+	def test_node_is_added_to_tail(self):
+		ll.add_to_tail(3)
+		assert ll.get_tail().value == 3
+
+	def test_add_at_position():
+		ll.add_to_head(4)
+		add_at_position()
 
 
 def run_tests():
